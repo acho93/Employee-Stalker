5d3430c491b6780742e964bc2dfedcd312ccf21d
Ashley Cho <ashleycho.slp@gmail.com>
2022-01-13 05:26:34 -0800

    add view functions inquirer

--- server.js
+++ server.js
@@ -5,14 +5,102 @@ const cTable = require('console.table');
 // Start server after DB connection
 db.connect(err => {
     if (err) throw err;
-    console.log("Connected as Id" + connection.threadId)
+    console.log('Database connected.');
     promptUser();
   });
 
 const promptUser = () => {
-    return inquirer.prompt([
+    inquirer.prompt([
         {
-            
+            type: 'list',
+            name: 'choices',
+            message: 'What would you like to do?',
+            choices: [
+                'View all departments',
+                'View all roles',
+                'View all employees',
+                'Add a department',
+                'Add a role',
+                'Add an employee',
+                'Update an employee role',
+                'Exit'
+            ]
         }
     ])
-}
+    .then(response => {
+        const { choices } = response;
+
+        if (choices === 'View all departments') {
+            viewDepartments();
+        }
+
+        if (choices === 'View all roles') {
+            viewRoles();
+        }
+
+        if (choices === 'View all employees') {
+            viewEmployees();
+        }
+
+        // if (choices === 'Add a department') {
+        //     addDepartment();
+        // }
+    
+        // if (choices === 'Add a role') {
+        //     addRole();
+        // }
+    
+        // if (choices === 'Add an employee') {
+        //     addEmployee();
+        // }
+    
+        // if (choices === 'Update an employee role') {
+        //     updateEmployee();
+        // }
+
+        // if (choices === 'Exit') {
+        //     db.end()
+        // };
+    });
+};
+
+viewDepartments = () => {
+    const sql = 'SELECT * FROM department'; 
+  
+    db.query(sql, (err, rows) => {
+      if (err) throw err;
+      console.table(rows);
+      promptUser();
+    });
+  };
+
+
+// viewDepartments = () => {
+//     const sql = `SELECT department.id AS id, department.name AS department FROM department`; 
+  
+//     db.promise().query(sql, (err, rows) => {
+//       if (err) throw err;
+//       console.table(rows);
+//       promptUser();
+//     });
+//   };
+
+viewRoles = () => {
+    const sql = 'SELECT * FROM role'; 
+  
+    db.query(sql, (err, rows) => {
+      if (err) throw err;
+      console.table(rows);
+      promptUser();
+    });
+  };
+
+viewEmployees = () => {
+    const sql = 'SELECT * FROM employee'; 
+  
+    db.query(sql, (err, rows) => {
+      if (err) throw err;
+      console.table(rows);
+      promptUser();
+    });
+  };
